e573bf2983613de2a5ee4cdba968e252b3304d55
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-06-05 17:43:52 +0530

    commit 1

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+@Test
+public class AppTest
+{
+    public void Test()
+    {
+        System.out.println("Test case is pass");
+    }
+}
